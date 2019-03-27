60368eeda68b18400b733754dc32b35d29ab05e7
thehicksivist <hicks.a.james@gmail.com>
2019-03-27 11:08:43 +1000

    added cors for react connection

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+const db = require('./queries')
 const app = express()
 const port = 3001
-const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
@@ -10,6 +11,8 @@ app.use(
     extended: true,
     })
 )
+
+app.use('/', cors());
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
